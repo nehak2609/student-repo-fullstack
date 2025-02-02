1f73dfcd7c9056e1fec6d9374963a1c867a182dd
Neha Khawle <nehak@pdx.edu>
2025-02-02 14:17:42 -0800

    Updated coin program to add pennies case

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -29,9 +29,15 @@ function calculateChange(amount) {
   for (let coin of coins) {
     let moneyCount = Math.floor(remainingAmount / coin.value);
     if (moneyCount > 0) {
-      totalResult.push(
-        `${moneyCount} ${coin.name}${moneyCount > 1 ? "s" : ""}`
-      );
+      let coinName =
+        coin.name === "penny"
+          ? moneyCount > 1
+            ? "pennies"
+            : "penny"
+          : moneyCount > 1
+          ? coin.name + "s"
+          : coin.name;
+      totalResult.push(`${moneyCount} ${coinName}`);
       remainingAmount -= moneyCount * coin.value; // Subtract used amount
     }
   }
@@ -41,10 +47,10 @@ function calculateChange(amount) {
 }
 
 // Sample test cases
-console.log(calculateChange(4.62));
-// $4.62 ==> 4 dollars, 2 quarters, 1 dime, 2 pennies
-console.log(calculateChange(0.16));
-// $0.16 ==> 1 dime, 1 nickel, 1 penny
+console.log(calculateChange(4.63));
+// $4.62 ==> 4 dollars, 2 quarters, 1 dime, 3pennies
+console.log(calculateChange(0.17));
+// $0.16 ==> 1 dime, 1 nickel, 2 penny
 console.log(calculateChange(150.11));
 // $150.11 ==> Error: the number is too large
 
